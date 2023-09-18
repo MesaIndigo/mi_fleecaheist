4823968635a8866b427d57b56587223b3f409864
Agimir <116332087+MIAgimir@users.noreply.github.com>
2023-09-18 10:56:15 -0700

    added en.lua locale

---
+++ locales/en.lua
@@ -0,0 +1,36 @@
+return {
+    -- start heist
+    bank_choice = 'Choose a bank',
+    bank_start_ntf = 'CNS: Bank system isolated',
+    bank_list = 'Heist Plan: Fleeca Bank',
+    bankop_alta = 'Los Santos, Alta St & Harwick Ave',
+    bankop_legion = 'Los Santos, Mission Row & Straberry Ave',
+    bankop_burton = 'Los Santos, Burton St & San Vitus Blvd',
+    bankop_delperro = 'Los Santos, Rockford Hills & Blvd Del Perro',
+    bankop_chumash = 'Chumash, Banham Canyon & Great Ocean Highway',
+    bankop_harmony = 'Harmony, Route 68 & Grand Senora Desert',
+    -- money carts
+    money_steal = 'Steal money',
+    money_stolen = 'You\'ve taken all there is',
+    -- manager
+    mngr_false1 = 'Manager on Vataction',
+    mngr_false2 = 'One less thing to worry about right?',
+    mngr_card_bribe = 'Bribe: security card',
+    mngr_card_take = 'Intimidate: security card',
+    mngr_card_kill = 'Retrieve: security card',
+    -- cameras
+    cam_warn = 'Watch out for the cameras',
+    cam_spot = 'You\'ve been spotted by the cameras',
+    cam_tool = 'Use: heist tools',
+    cam_dsbl = 'Camera system has been disabled',
+    -- security
+    terminal_prog = 'Hacking terminal',
+    terminal_hack = 'Hack into bank systems',
+    terminal_card = 'Use: security card',
+    -- vault
+    vault_open = 'Open the vaul',
+    vault_opening = 'Vault door: opening',
+    vault_close = 'Close the vault',
+    vault_closing = 'Vault door: closing',
+    vault_drill = 'Use: thermal drill',
+}
